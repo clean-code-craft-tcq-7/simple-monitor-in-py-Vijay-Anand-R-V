52e9effd1d1bafe2684c9d55ae4f9560a4221113
Ramachandriya Vijaya Sekar Vijay Anand (MS/EHM-BST-TS) <xvm1cob@bosch.com>
2025-07-29 10:37:27 +0530

    updated the monitor.py file

--- monitor.py
+++ monitor.py
@@ -2,36 +2,38 @@
 from time import sleep
 import sys
 
+def print_info():
+  for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
 
-def vitals_ok(temperature, pulseRate, spo2):
+def check_temperature(temperature):
   if temperature > 102 or temperature < 95:
     print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    print_info()
     return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
+
   return True
+
+def check_pulseRate(pulseRate):
+  if pulseRate < 60 or pulseRate > 100:
+    print('Pulse Rate is out of range!')
+    print_info()
+    return False
+
+  return True
+
+def check_spo2(spo2):
+  if spo2 < 90:
+    print('Oxygen Saturation out of range!')
+    print_info()
+    return False
+
+  return True
+
+def vitals_ok(temperature, pulseRate, spo2):
+  return check_temperature(temperature) & check_pulseRate(pulseRate) & check_spo2(spo2)
